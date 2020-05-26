d7604af92793fdfd1e71f31f60dfd30da0cf8212
sergapit1 <serg.apit0@gmail.com>
2020-05-27 00:48:26 +0300

    Dependency Injection

--- src/main/java/by/sergapit/javaspring/TestSpring.java
+++ src/main/java/by/sergapit/javaspring/TestSpring.java
@@ -8,8 +8,11 @@ public class TestSpring {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+      //  Music music = context.getBean("musicBean", Music.class);
+       // MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
+
+
         musicPlayer.playMusic();
 
 
