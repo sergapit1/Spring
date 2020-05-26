c9ef2750aefdc6a23564e9de1fe940be3684292e
sergapit1 <serg.apit0@gmail.com>
2020-05-27 00:38:55 +0300

    spring3

--- src/main/java/by/sergapit/javaspring/TestSpring.java
+++ src/main/java/by/sergapit/javaspring/TestSpring.java
@@ -9,7 +9,10 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Music music = context.getBean("musicBean", Music.class);
-        System.out.println(testBean.getName());
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+
+
         context.close();
     }
 }
