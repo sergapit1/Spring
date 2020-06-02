23c5b9de2f092885b492e6d54018cadfc02a9650
sergapit1 <serg.apit0@gmail.com>
2020-06-02 22:58:30 +0300

    comporasion

--- src/main/java/by/sergapit/javaspring/TestSpring.java
+++ src/main/java/by/sergapit/javaspring/TestSpring.java
@@ -10,12 +10,18 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
       //  Music music = context.getBean("musicBean", Music.class);
        // MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer firstMusicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer secondMusicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
+boolean comporarison = firstMusicPlayer == secondMusicPlayer;
 
+        //musicPlayer.playMusic();
+        System.out.println(comporarison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
 
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
